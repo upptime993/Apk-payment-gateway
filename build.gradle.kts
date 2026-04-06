302e2b26d920fb041fcacd79993fe9b633f54bd7
upptime993 <ikbalrakanheheheh@gmail.com>
2026-04-06 07:54:36 +0700

    Delete build.gradle.kts

--- build.gradle.kts
+++
@@ -1,5 +0,0 @@
-plugins {
-    autowire(libs.plugins.android.application) apply false
-    autowire(libs.plugins.android.library) apply false
-    autowire(libs.plugins.kotlin.android) apply false
-}
